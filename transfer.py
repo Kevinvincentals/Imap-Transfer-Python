73783525b7ebcc565dc81d51a4064ab9b1644fba
Kevin Vincent Als <kevin_als@hotmail.com>
2023-07-06 18:09:20 +0200

    Adding of email backups (local)

--- transfer.py
+++ transfer.py
@@ -1,3 +1,5 @@
+import os
+import zipfile
 from getpass import getpass
 from imapclient import IMAPClient
 from email import message_from_bytes
@@ -28,15 +30,29 @@ def match_mailboxes(source_client, dest_client):
                 break
     return matches
 
+# Backup option
+backup_option = input("Choose an option:\n1. Transfer emails\n2. Backup emails\nEnter your choice: ")
+while backup_option not in ['1', '2', '3']:
+    print("Invalid choice, please try again.")
+    backup_option = input("Choose an option:\n1. Transfer emails\n2. Backup emails\nEnter your choice: ")
+
+
 # Source account details
 source_host = input("Enter the source host (IMAP server): ")
 source_username = input("Enter the source username: ")
 source_password = getpass("Enter the source password: ")
 
-# Destination account details
-dest_host = input("Enter the destination host (IMAP server): ")
-dest_username = input("Enter the destination username: ")
-dest_password = getpass("Enter the destination password: ")
+# Destination account details if transfer option is chosen
+if backup_option == '1':
+    dest_host = input("Enter the destination host (IMAP server): ")
+    dest_username = input("Enter the destination username: ")
+    dest_password = getpass("Enter the destination password: ")
+
+# Prepare backup file if backup option is chosen
+if backup_option == '2':
+    backup_password = getpass("Enter a password for the backup file: ")
+    backup_filename = "email_backup.zip"
+    backup_mailboxes = []
 
 # Function to try to connect with SSL, then with TLS if SSL fails
 def connect_imap(host, username, password):
@@ -49,22 +65,79 @@ def connect_imap(host, username, password):
             continue
     raise ValueError(f"Could not connect to {host} with the provided credentials")
 
-# Connect to the servers
-with connect_imap(source_host, source_username, source_password) as source_client, \
-     connect_imap(dest_host, dest_username, dest_password) as dest_client:
+if backup_option == '1':
+    # Connect to the servers
+    with connect_imap(source_host, source_username, source_password) as source_client, \
+         connect_imap(dest_host, dest_username, dest_password) as dest_client:
 
-    auto_move = input("Do you want to automatically match mailboxes and move emails? (y/n): ").lower().strip() == "y"
+        auto_move = input("Do you want to automatically match mailboxes and move emails? (y/n): ").lower().strip() == "y"
 
-    if auto_move:
-        matches = match_mailboxes(source_client, dest_client)
-        if not matches:
-            print("No matching mailboxes found.")
-            exit()
+        if auto_move:
+            matches = match_mailboxes(source_client, dest_client)
+            if not matches:
+                print("No matching mailboxes found.")
+                exit()
 
-        for src_box, dest_box in matches:
-            print(f"\nMoving emails from {src_box} to {dest_box}...")
-            source_client.select_folder(src_box)
-            dest_client.select_folder(dest_box)
+            for src_box, dest_box in matches:
+                print(f"\nMoving emails from {src_box} to {dest_box}...")
+                source_client.select_folder(src_box)
+                dest_client.select_folder(dest_box)
+
+                # Fetch all message IDs from source mailbox
+                source_messages = source_client.search('ALL')
+
+                # Fetch all message IDs from destination mailbox
+                dest_messages = dest_client.search('ALL')
+
+                # Get the message IDs already present in the destination mailbox
+                dest_message_ids = set(dest_messages)
+
+                # Filter out the already transferred messages from the source mailbox
+                messages = [msg_id for msg_id in source_messages if msg_id not in dest_message_ids]
+
+                print(f"Total emails to be copied: {len(messages)}")
+
+                if len(messages) > 4000:
+                    print("Due to the large quantity of emails, this may take some time. Please wait...")
+
+                # Get the full message data (including FLAGS) for the messages
+                response = source_client.fetch(messages, ['BODY.PEEK[]', 'FLAGS', 'RFC822.SIZE'])
+
+                transferred_count = 0
+                duplicate_count = 0
+                total_size = 0
+
+                with tqdm(total=len(response), desc="Copying emails", unit="email", ncols=80) as pbar:
+                    for msgid in response.keys():
+                        raw_message = response[msgid][b'BODY[]']
+                        message = message_from_bytes(raw_message)
+                        flags = response[msgid][b'FLAGS']
+                        size = response[msgid][b'RFC822.SIZE']
+                        total_size += size
+
+                        # Check if the message is already present in the destination mailbox
+                        message_id = message['Message-ID'].strip() if message['Message-ID'] else ''
+                        dest_search = dest_client.search(['HEADER', 'Message-ID', message_id]) if message_id else []
+
+                        if dest_search:
+                            duplicate_count += 1
+                        else:
+                            dest_client.append(dest_box, message.as_bytes(), flags=flags)
+                            transferred_count += 1
+
+                        pbar.set_postfix({"Size moved:": f"{total_size/(1024*1024):.2f} MB"})
+                        pbar.update(1)
+
+                print(f"\n{transferred_count} messages copied from {src_box} to {dest_box}.")
+                print(f"{duplicate_count} duplicate messages skipped.")
+                print(f"Total size of moved emails: {total_size / (1024 * 1024):.2f} MB")
+
+        else:
+            source_mailbox = choose_mailbox(source_client, "Source mailboxes:")
+            dest_mailbox = choose_mailbox(dest_client, "Destination mailboxes:")
+
+            source_client.select_folder(source_mailbox)
+            dest_client.select_folder(dest_mailbox)
 
             # Fetch all message IDs from source mailbox
             source_messages = source_client.search('ALL')
@@ -78,7 +151,7 @@ with connect_imap(source_host, source_username, source_password) as source_clien
             # Filter out the already transferred messages from the source mailbox
             messages = [msg_id for msg_id in source_messages if msg_id not in dest_message_ids]
 
-            print(f"Total emails to be copied: {len(messages)}")
+            print(f"\nTotal emails to be copied: {len(messages)}")
 
             if len(messages) > 4000:
                 print("Due to the large quantity of emails, this may take some time. Please wait...")
@@ -91,81 +164,106 @@ with connect_imap(source_host, source_username, source_password) as source_clien
             total_size = 0
 
             with tqdm(total=len(response), desc="Copying emails", unit="email", ncols=80) as pbar:
-                for msgid in response.keys():
-                    raw_message = response[msgid][b'BODY[]']
-                    message = message_from_bytes(raw_message)
-                    flags = response[msgid][b'FLAGS']
-                    size = response[msgid][b'RFC822.SIZE']
-                    total_size += size
+                    for msgid in response.keys():
+                        raw_message = response[msgid][b'BODY[]']
+                        message = message_from_bytes(raw_message)
+                        flags = response[msgid][b'FLAGS']
+                        size = response[msgid][b'RFC822.SIZE']
+                        total_size += size
 
-                    # Check if the message is already present in the destination mailbox
-                    message_id = message['Message-ID'].strip() if message['Message-ID'] else ''
-                    dest_search = dest_client.search(['HEADER', 'Message-ID', message_id]) if message_id else []
+                        # Check if the message is already present in the destination mailbox
+                        message_id = message['Message-ID'].strip() if message['Message-ID'] else ''
+                        dest_search = dest_client.search(['HEADER', 'Message-ID', message_id]) if message_id else []
 
-                    if dest_search:
-                        duplicate_count += 1
-                    else:
-                        dest_client.append(dest_box, message.as_bytes(), flags=flags)
-                        transferred_count += 1
+                        if dest_search:
+                            duplicate_count += 1
+                        else:
+                            dest_client.append(dest_mailbox, message.as_bytes(), flags=flags)
+                            transferred_count += 1
 
-                    pbar.set_postfix({"Size moved:": f"{total_size/(1024*1024):.2f} MB"})
-                    pbar.update(1)
+                        pbar.set_postfix({"Transferred": transferred_count, "Duplicates": duplicate_count, "Total Size": f"{total_size/(1024*1024):.2f} MB"})
+                        pbar.update(1)
 
-            print(f"\n{transferred_count} messages copied from {src_box} to {dest_box}.")
+            print(f"\n{transferred_count} messages copied from {source_mailbox} to {dest_mailbox}.")
             print(f"{duplicate_count} duplicate messages skipped.")
             print(f"Total size of moved emails: {total_size / (1024 * 1024):.2f} MB")
 
-    else:
-        source_mailbox = choose_mailbox(source_client, "Source mailboxes:")
-        dest_mailbox = choose_mailbox(dest_client, "Destination mailboxes:")
+        # Summary
+        print("\n--- Summary ---")
+        if auto_move:
+            for src_box, dest_box in matches:
+                print(f"Moved from Source {src_box} to Destination {dest_box}:")
+                print(f"  - Emails moved: {transferred_count}")
+                print(f"  - Duplicate messages skipped: {duplicate_count}")
+                print(f"  - Total size: {total_size / (1024 * 1024):.2f} MB")
+                print()
+        else:
+            print(f"Moved from Source {source_mailbox} to Destination {dest_mailbox}:")
+            print(f"  - Emails moved: {transferred_count}")
+            print(f"  - Duplicate messages skipped: {duplicate_count}")
+            print(f"  - Total size: {total_size / (1024 * 1024):.2f} MB")
 
-        source_client.select_folder(source_mailbox)
-        dest_client.select_folder(dest_mailbox)
+elif backup_option == '2':
+    # Connect to the source server for backup
+    with connect_imap(source_host, source_username, source_password) as source_client:
+        # Get the list of source mailboxes
+        source_mailboxes = [box[-1] for box in source_client.list_folders()]
 
-        # Fetch all message IDs from source mailbox
-        source_messages = source_client.search('ALL')
+        # Select specific mailboxes for backup
+        print("\nSelect the mailboxes to backup:")
+        for i, mailbox in enumerate(source_mailboxes, 1):
+            print(f"{i}. {mailbox}")
+        print("Enter the mailbox numbers to backup (comma-separated): ")
+        choices = input("Choose mailboxes: ").split(",")
+        for choice in choices:
+            choice = choice.strip()
+            if choice.isdigit() and 1 <= int(choice) <= len(source_mailboxes):
+                backup_mailboxes.append(source_mailboxes[int(choice) - 1])
+            else:
+                print(f"Invalid choice: {choice}. Skipping.")
 
-        # Fetch all message IDs from destination mailbox
-        dest_messages = dest_client.search('ALL')
+        if not backup_mailboxes:
+            print("No mailboxes selected for backup. Exiting.")
+            exit()
 
-        # Get the message IDs already present in the destination mailbox
-        dest_message_ids = set(dest_messages)
+        # Connect to the source server again to perform backup
+        with connect_imap(source_host, source_username, source_password) as source_client:
+            print(f"\nBacking up {len(backup_mailboxes)} mailboxes...")
 
-        # Filter out the already transferred messages from the source mailbox
-        messages = [msg_id for msg_id in source_messages if msg_id not in dest_message_ids]
+            with zipfile.ZipFile(backup_filename, "w", zipfile.ZIP_DEFLATED) as backup_zip:
+                backup_count = 0
 
-        print(f"\nTotal emails to be copied: {len(messages)}")
+                for mailbox in tqdm(backup_mailboxes, desc="Backing up mailboxes", ncols=80):
+                    source_client.select_folder(mailbox)
 
-        if len(messages) > 4000:
-            print("Due to the large quantity of emails, this may take some time. Please wait...")
+                    # Fetch all message IDs from the mailbox
+                    messages = source_client.search('ALL')
 
-        # Get the full message data (including FLAGS) for the messages
-        response = source_client.fetch(messages, ['BODY.PEEK[]', 'FLAGS', 'RFC822.SIZE'])
+                    # Fetch the full message data for the messages
+                    response = source_client.fetch(messages, ['BODY.PEEK[]'])
 
-        transferred_count = 0
-        duplicate_count = 0
-        total_size = 0
+                    for msgid, data in response.items():
+                        raw_message = data[b'BODY[]']
+                        message = message_from_bytes(raw_message)
 
-        with tqdm(total=len(response), desc="Copying emails", unit="email", ncols=80) as pbar:
-            for msgid in response.keys():
-                raw_message = response[msgid][b'BODY[]']
-                message = message_from_bytes(raw_message)
-                flags = response[msgid][b'FLAGS']
-                size = response[msgid][b'RFC822.SIZE']
-                total_size += size
+                        # Create a folder for the current message within the backup zip file
+                        message_folder = f"{mailbox}/{msgid}"
+                        backup_zip.writestr(f"{message_folder}/message.eml", raw_message)
 
-                # Check if the message is already present in the destination mailbox
-                message_id = message['Message-ID'].strip() if message['Message-ID'] else ''
-                dest_search = dest_client.search(['HEADER', 'Message-ID', message_id]) if message_id else []
-                if dest_search:
-                    duplicate_count += 1
-                else:
-                    dest_client.append(dest_mailbox, message.as_bytes(), flags=flags)
-                    transferred_count += 1
+                        # Save attachments
+                        for part in message.walk():
+                            if part.get_content_maintype() == "multipart":
+                                continue
+                            if part.get("Content-Disposition") is None:
+                                continue
 
-                pbar.set_postfix({"Transferred": transferred_count, "Duplicates": duplicate_count, "Total Size": f"{total_size/(1024*1024):.2f} MB"})
-                pbar.update(1)
+                            filename = part.get_filename()
+                            if filename:
+                                attachment_data = part.get_payload(decode=True)
+                                backup_zip.writestr(f"{message_folder}/{filename}", attachment_data)
 
-        print(f"\n{transferred_count} messages copied from {source_mailbox} to {dest_mailbox}.")
-        print(f"{duplicate_count} duplicate messages skipped.")
-        print(f"Total size of moved emails: {total_size / (1024 * 1024):.2f}MB")
+                        backup_count += 1
+
+            print(f"\nBackup created successfully: {backup_filename}")
+            print(f"Total emails backed up: {backup_count}")
+            print('Please note, there is currently no logic to handle importing of emails')
